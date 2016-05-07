2f664fee54204b85af659e1bd807dc6dc3b1839b
Kevin Thrailkill <kevinjt92@gmail.com>
2016-05-07 12:28:02 -0700

    Bloc Browser Initial Project Setup

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Kevin Thrailkill on 5/7/16.
+//  Copyright © 2016 kevinthrailkill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Kevin Thrailkill on 5/7/16.
+//  Copyright © 2016 kevinthrailkill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
